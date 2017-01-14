f40a8908b3a04a7a5a03af3c3b119bb4b8c1b216
Bijiabo <bijiabo@gmail.com>
2017-01-14 20:49:32 +0800

    更新 navigation bar 显示内容和部分元素样式

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,9 @@
 /**
  * Created by huchunbo on 2017/1/12.
  */
+$(document).on('focus', '.navigation-bar .form-control', function(){
+    $('.navigation-bar .input-group').addClass('focus');
+});
+$(document).on('blur', '.navigation-bar .form-control', function(){
+    $('.navigation-bar .input-group').removeClass('focus');
+});
