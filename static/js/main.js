02f2c8e1bd284409c75f8b29f36a4b40e25ae4c9
Bijiabo <bijiabo@gmail.com>
2017-01-15 11:17:08 +0800

    优化评论区域页面显示和交互

--- static/js/main.js
+++ static/js/main.js
@@ -7,3 +7,21 @@ $(document).on('focus', '.navigation-bar .form-control', function(){
 $(document).on('blur', '.navigation-bar .form-control', function(){
     $('.navigation-bar .input-group').removeClass('focus');
 });
+// 评论区域交互
+// 响应回复点击
+$(document).on('click', '.reply-button', function () {
+    var parent = $($(this).parent());
+    var replyInputContainerElementClass = '.comment-post-input-container';
+    var replyInputCOntainerHTML = '\
+        <div class="comment-post-input-container reply-append-input-container">\
+            <textarea class="col-md-12" name="comment" cols="30" rows="3" placeholder="点此输入评论内容..."></textarea>\
+            <button class="btn btn-default col-md-12">发表</button>\
+        </div>';
+
+    if (parent.has(replyInputContainerElementClass).length === 0) {
+        $('.reply-append-input-container').remove();
+        parent.append(replyInputCOntainerHTML);
+    } else {
+        parent.find(replyInputContainerElementClass).remove();
+    }
+});
