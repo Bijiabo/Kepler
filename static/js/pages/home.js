1eb6a3b12b85cfc96c1952c1510a8a0c1bab659c
Bijiabo <bijiabo@gmail.com>
2017-04-06 02:01:45 +0800

    - 调整页面结构 - 优化模板加载逻辑

---
+++ static/js/pages/home.js
@@ -0,0 +1,16 @@
+/**
+ * Created by huchunbo on 2017/4/6.
+ */
+// 渲染元素
+var render = function() {
+    var app = new Vue({
+        el: '#app',
+        data: {
+            videoCards: Array.apply(null, Array(50))
+        },
+        methods: {
+        },
+        mounted: function () {
+        }
+    });
+};
---
+++ static/js/template-loader.js
@@ -0,0 +1,42 @@
+/**
+ * Created by huchunbo on 2017/4/6.
+ */
+
+var loadTemplateCount = 0;
+
+var loadTemplateUnit = function (element, callback) {
+    var templateImports = element.find('template-import');
+    $.each(templateImports, function(index, item) {
+        item = $(item);
+        var contentHTMLCache = item.html();
+        var templateUrl = item.attr('file');
+        loadTemplateCount++;
+        $.get(templateUrl, function(result) {
+            loadTemplateCount--;
+            item.html(result);
+            
+            // 获取 slot
+            var slot = item.find('template-slot');
+            if (slot.length > 0 && contentHTMLCache.length > 0) {
+                slot.html(contentHTMLCache);
+                loadTemplateUnit(slot, callback);
+            }
+            
+            console.log(loadTemplateCount);
+            // 判断是否执行渲染
+            if (loadTemplateCount == 0) {
+                callback();
+            }
+        });
+    });
+};
+
+$(function () {
+    // setTimeout(loadTemplates, 1000);
+    setTimeout(function () {
+        loadTemplateUnit($('body'), function () {
+            console.info('done');
+            if (window.render !== undefined) { window.render(); }
+        });
+    }, 1000);
+});
