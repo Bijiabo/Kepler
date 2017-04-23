d3234a3d5f6ec2701b12e482029b6f09ba84d3f9
Bijiabo <bijiabo@gmail.com>
2017-04-23 22:06:02 +0800

    添加搜索历史界面

--- sumred/static/js/pages/history-search.js
+++ sumred/static/js/pages/history-search.js
@@ -8,11 +8,14 @@ var render = function() {
     var app = new Vue({
         el: '#app',
         data: {
-            videoCards: Array.apply(null, Array(50))
+            watchHistory: Array.apply(null, Array(10)).map(function(item, i) {
+                return 0;
+            })
         },
         methods: {
         },
         mounted: function () {
+            window.renderContext = this;
         }
     });
 };
@@ -24,5 +27,27 @@ requirejs(['public'], function(_public) {
     };
     
     window.didLoadActions.push(targetActions);
+    
+    var bindEvents = function () {
+        // 用户删除搜索记录条目
+        $(document).on('click', '.list-video-card .delete', function () {
+            var $this = $(this);
+            var targetId = $this.attr('target-id');
+            // todo: 调用接口删除搜索记录条目
+            $this.parents('.list-video-card-item-package').remove();
+        });
+        
+        // 用户点击加载更多
+        $(document).on('click', '.load-more', function (e) {
+            e.preventDefault();
+            // todo: 此处获取更多数据加载到页面中
+            // 模拟加载远程数据
+            var newData = Array.apply(null, Array(10)).map(function(item, i) {
+                return 0;
+            });
+            Vue.set(window.renderContext, 'watchHistory', window.renderContext.watchHistory.concat( newData ));
+        });
+    };
+    bindEvents();
     
 });
