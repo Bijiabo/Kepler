daff5d73b2b623be7bb8f892a4107e529812a542
Bijiabo <bijiabo@gmail.com>
2017-04-24 00:18:20 +0800

    完成频道浏览界面头部轮播组件

---
+++ sumred/static/js/pages/channel.js
@@ -0,0 +1,28 @@
+/**
+ * Created by huchunbo on 2017/4/6.
+ */
+window.didLoadActions = [];
+
+// 渲染元素
+var render = function() {
+    var app = new Vue({
+        el: '#app',
+        data: {
+            videoCards: Array.apply(null, Array(50))
+        },
+        methods: {
+        },
+        mounted: function () {
+        }
+    });
+};
+window.didLoadActions.push(render);
+
+requirejs(['public'], function(_public) {
+    var targetActions = function () {
+        _public.init();
+    };
+    
+    window.didLoadActions.push(targetActions);
+    
+});
