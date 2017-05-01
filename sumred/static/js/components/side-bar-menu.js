5803f2ddf86f1b5400a1a2f23d8db9efa0d31613
Bijiabo <bijiabo@gmail.com>
2017-05-01 16:32:14 +0800

    + 侧边栏添加、删除频道效果 -> 与 Youtube 一样

--- sumred/static/js/components/side-bar-menu.js
+++ sumred/static/js/components/side-bar-menu.js
@@ -37,18 +37,41 @@ define([], function () {
         // 测试添加频道
         $(document).on('click', '.test-book-channel', function () {
             var itemHTML = '\
-            <a href="#">\
-            <div class="item">\
-                <div class="logo">\
-                    <img src="../static/image/avatars/3.jpg" alt="">\
+                <a href="#">\
+                <div class="item adding" title="测试频道">\
+                    <div class="logo">\
+                        <img src="../static/image/avatars/3.jpg" alt="">\
+                    </div>\
+                    <div class="title">测试频道</div>\
+                    <div class="info">1024+</div>\
                 </div>\
-                <div class="title">测试频道</div>\
-                <div class="info">1024+</div>\
-            </div>\
-            </a>\
+                </a>\
             ';
-            $('.side-bar-menu .channel-list').prepend(itemHTML);
-            $('.side-bar-menu .book-channel-success').show().fadeOut(1000);
+            var newItem = $(itemHTML);
+            $('.side-bar-menu .channel-list').prepend(newItem);
+            setTimeout(function () {
+                newItem.find('.item').removeClass('adding');
+            }, 1900);
+        });
+    
+        // 测试取消订阅频道
+        $(document).on('click', '.test-cancel-book-channel', function () {
+            var channelListElment = $('.side-bar-menu .channel-list');
+            if (channelListElment.find('.item').length == 0) { return; }
+            // 随机选择一个要删除的目标
+            var targetItem = $(
+                channelListElment.find('a').get(
+                    Math.floor(
+                        Math.random() * channelListElment.find('.item').length
+                    )
+                )
+            );
+            console.log(targetItem);
+            targetItem.find('.item').addClass('removing');
+            
+            setTimeout(function () {
+                targetItem.remove();
+            }, 1500);
         });
     };
     
