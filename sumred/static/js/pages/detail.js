d0507154d508d6690d784b27717144c2d539a33a
Bijiabo <bijiabo@gmail.com>
2017-05-09 23:56:06 +0800

    更新播放器样式

--- sumred/static/js/pages/detail.js
+++ sumred/static/js/pages/detail.js
@@ -24,13 +24,27 @@ requirejs(['public'], function(_public) {
         // video play
         var controls = ["<div class='plyr__controls'>",
             "<button type='button' data-plyr='play'>",
-            "<svg><use xlink:href='#plyr-play'></use></svg>",
+            "<span class='iconfont'>&#xe61a;</span>",
             "<span class='plyr__sr-only'>Play</span>",
             "</button>",
             "<button type='button' data-plyr='pause'>",
-            "<svg><use xlink:href='#plyr-pause'></use></svg>",
+            "<span class='iconfont'>&#xe61b;</span>",
             "<span class='plyr__sr-only'>Pause</span>",
             "</button>",
+            
+            "<div class='volume-group'>",
+            "<button type='button' data-plyr='mute' class='toggle-mute'>",
+            "<span class='iconfont icon--muted'>&#xe615;</span>",
+            "<span class='iconfont'>&#xe617;</span>",
+            "<span class='plyr__sr-only'>Toggle Muste</span>",
+            "</button>",
+            "<span class='plyr__volume'>",
+            "<label for='volume{id}' class='plyr__sr-only'>Volume</label>",
+            "<input id='volume{id}' class='plyr__volume--input' type='range' min='0' max='10' value='5' data-plyr='volume'>",
+            "<progress class='plyr__volume--display' max='10' value='0' role='presentation'></progress>",
+            "</span>",
+            "</div>",
+            
             "<span class='plyr__progress'>",
             "<label for='seek{id}' class='plyr__sr-only'>Seek</label>",
             "<input id='seek{id}' class='plyr__progress--seek' type='range' min='0' max='100' step='0.1' value='0' data-plyr='seek'>",
@@ -48,34 +62,35 @@ requirejs(['public'], function(_public) {
             "<span class='plyr__sr-only'>Duration</span>",
             "<span class='plyr__time--duration'>00:00</span>",
             "</span>",
-            "<button type='button' data-plyr='mute'>",
-            "<svg class='icon--muted'><use xlink:href='#plyr-muted'></use></svg>",
-            "<svg><use xlink:href='#plyr-volume'></use></svg>",
-            "<span class='plyr__sr-only'>Toggle Mute</span>",
-            "</button>",
-            "<span class='plyr__volume'>",
-            "<label for='volume{id}' class='plyr__sr-only'>Volume</label>",
-            "<input id='volume{id}' class='plyr__volume--input' type='range' min='0' max='10' value='5' data-plyr='volume'>",
-            "<progress class='plyr__volume--display' max='10' value='0' role='presentation'></progress>",
-            "</span>",
+            
             "<div class='right-container'>",
+    
+            // 分辨率切换
+            "<button type='button' class='toggle-ratio-mode'>",
+            "<span class='iconfont'>1080P</span>",
+            "<span class='plyr__sr-only'>toggle ratio mode</span>",
+            "</button>",
+            
             // 剧场模式切换
             "<button type='button' class='toggle-theatre-mode'>",
-            // "<svg class='icon--captions-on'><use xlink:href='#plyr-captions-on'></use></svg>",
-            "<div>[Theatre Mode]</div>",
-            // "<svg><use xlink:href='#plyr-captions-off'></use></svg>",
-            "<span class='plyr__sr-only'>Toggle Theatre Mode</span>",
+            "<span class='iconfont'>&#xe613;</span>",
+            "<span class='plyr__sr-only'>toggle theatre mode</span>",
             "</button>",
-            "<button type='button' data-plyr='captions'>",
-            "<svg class='icon--captions-on'><use xlink:href='#plyr-captions-on'></use></svg>",
-            "<svg><use xlink:href='#plyr-captions-off'></use></svg>",
+            
+            // 字幕切换
+            "<button type='button' data-plyr='captions' class='toggle-captions-btn'>",
+            "<span class='icon--captions-on iconfont'>&#xe612;</span>",
+            "<span class='iconfont'>&#xe612;</span>",
             "<span class='plyr__sr-only'>Toggle Captions</span>",
             "</button>",
+            
+            // 全屏幕切换
             "<button type='button' data-plyr='fullscreen'>",
-            "<svg class='icon--exit-fullscreen'><use xlink:href='#plyr-exit-fullscreen'></use></svg>",
-            "<svg><use xlink:href='#plyr-enter-fullscreen'></use></svg>",
+            "<span class='icon--exit-fullscreen iconfont'>&#xe611;</span>",
+            "<span class='iconfont'>&#xe614;</span>",
             "<span class='plyr__sr-only'>Toggle Fullscreen</span>",
             "</button>",
+            
             "</div>",// .right-container
             "</div>"].join("");
         var playerOptions = {
@@ -105,6 +120,15 @@ requirejs(['public'], function(_public) {
             }
             
         });
+        /*
+        $(document).on('mouseenter', '.toggle-mute', function () {
+            $('.plyr--video .plyr__controls .plyr__volume').show();
+        });
+    
+        $(document).on('mouseleave', '.toggle-mute', function () {
+            $('.plyr--video .plyr__controls .plyr__volume').hide();
+        });
+        */
     };
     bindEvents();
     
