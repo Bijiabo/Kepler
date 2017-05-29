d97939ec3a5dab6abacf92a19cfd3f9d5f151c9d
Bijiabo <bijiabo@gmail.com>
2017-05-29 12:37:10 +0800

    优化消息盒子样式和逻辑

--- samured/static/js/components/message-box.js
+++ samured/static/js/components/message-box.js
@@ -4,7 +4,9 @@
 define([], function () {
     
     var init = function () {
-        element.content().html(generate.list());
+        generate.list(function (result) {
+            element.content().html(result);
+        });
         bindEvents();
     };
     
@@ -16,13 +18,18 @@ define([], function () {
         $(document).on('click', '.message-box .content .message-list-item', function () {
             // load message item content
             targetIndex = $(this).attr('index');
-            element.content().html(generate.content());
+            generate.content(targetIndex, function (result) {
+                element.content().html(result);
+            });
+            
             element.title().html(generate.title.content());
         });
         
         // back to list page
         $(document).on('click', '.message-box .title .back', function () {
-            element.content().html(generate.list());
+            generate.list(function (result) {
+                element.content().html(result);
+            });
             element.title().html(generate.title.list());
         });
     };
@@ -37,12 +44,14 @@ define([], function () {
     };
     
     var generate = {
-        list: function () {
+        list: function (callback) {
             var result = '';
+            
+            // todo: 此处异步获取数据
             
             for (var i=0; i<6; i++) {
                 var cellCode = '\
-                <div class="message-item-cell message-list-item" index="'+i+'">\
+                <div class="message-item-cell message-list-item '+ (i===0 ? 'unread':'') +'" index="'+i+'">\
                         <!--<div class="preview">\
                             <img src="./../static/image/video-thumbnails/'+i+'.jpg" alt="">\
                         </div>-->\
@@ -56,28 +65,33 @@ define([], function () {
                 </div>';
                 result += cellCode;
             }
-            return result;
+            callback(result);
         },
-        content: function () {
+        content: function (targetId, callback) {
             var result = '';
-    
+            
+            // todo: 异步获取数据
+            var data = {
+                title: '账号数据异常',
+                time: '2017年3月29日上午 10:00',
+                content: 'All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie)\
+                          All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie)\
+                          All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie)\
+                          All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie)\
+                          <p>just a test...</p>'
+            };
+            
             var cellCode = '\
                 <div class="message-item-cell">\
                         <div class="content">\
-                            <div class="item-title">账号数据异常</div>\
-                            <div class="time">2017年3月29日上午 10:00</div>\
-                            <div class="content-display">\
-                                All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie)\
-                                All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie)\
-                                All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie)\
-                                All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie) All Cutsences (Game Movie)\
-                                <p>just a test...</p>\
-                            </div>\
+                            <div class="item-title text-color-blue">'+data.title+'</div>\
+                            <div class="time">'+data.time+'</div>\
+                            <div class="content-display">'+data.content+'</div>\
                         </div>\
                 </div>';
             result += cellCode;
             
-            return result;
+            callback(result);
         },
         title: {
             list: function () {
